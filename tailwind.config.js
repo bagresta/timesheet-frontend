5d817c5b17aa0e2d5504d43efe44054921b05ae6
Blaise Agresta <bagresta@hotmail.com>
2025-07-31 17:04:42 +1000

    Initial commit - Timesheet frontend

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",           // Your HTML file
+    "./src/**/*.{js,ts,jsx,tsx}", // Any JS/TS files in src/
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+
